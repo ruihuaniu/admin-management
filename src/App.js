7571fd6e2a7af285a7d69fa823a21350a9e98349
Barton NIU <barton.niu@gmail.com>
2020-03-21 16:41:25 +1100

    Change the folder structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Candidates from './components/Candidates'
+
+const App = ()=>{
+    
+    return(
+        <div>
+            <Candidates/>
+        </div>
+    )
+}
+
+
+export default App
